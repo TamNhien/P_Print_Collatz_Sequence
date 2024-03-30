f1ba341bc86f9020e7656038952c9130da0b44fa
TamNhien <canhthien91@gmail.com>
2024-03-30 21:20:01 +0700

    Add project files.

---
+++ P_Print_Collatz_Sequence.py
@@ -0,0 +1,14 @@
+﻿def collatz_conjecture(n):
+    sequence = [n]
+    while n != 1:
+        n = n // 2 if n % 2 == 0 else 3 * n + 1
+        sequence.append(n)
+    return sequence
+
+def print_collatz_sequence(m):
+    for i in range(1, m + 1):
+        print(*collatz_conjecture(i), sep=', ')
+
+m = int(input("Nhập vào số nguyên dương m: "))
+print_collatz_sequence(m)
+
